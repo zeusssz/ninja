706c2d68b6020e354d43c7f2ca3e74e4116727fc
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-14 19:27:44 +0530

    Delete src directory

--- src/utils.cpp
+++
@@ -1,9 +0,0 @@
-#include "utils.h"
-#include <fstream>
-
-void logError(const std::string& message) {
-    std::ofstream logFile("raven.log", std::ios_base::app);
-    if (logFile.is_open()) {
-        logFile << message << std::endl;
-    }
-}
--- src/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <string>
-
-void logError(const std::string& message);
-
-#endif // UTILS_H
